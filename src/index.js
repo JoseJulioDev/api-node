2284506f08aaacee724fc1dadc112a9396b4e20b
JoseJulioDev <josejulioumbelino.dev@gmail.com>
2020-12-28 21:46:08 -0300

    parameter methods

--- src/index.js
+++ src/index.js
@@ -2,18 +2,34 @@ const express = require("express");
 
 const app = express(); 
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.send("A API in node.js using express");
 });
 
+// params query
 app.get('/projects', (request, response) => {
+   
+    const {title, owner} = request.query;
+
+    console.log(title);
+    console.log(owner);
+   
     return response.json([
         'Projeto 1',
         'Projeto 2'
     ]);
 });
 
+// params body
 app.post('/projects', (request, response) => {
+    
+    const {title, owner} = request.body;
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -22,7 +38,13 @@ app.post('/projects', (request, response) => {
 });
 
 
+// params resquest
 app.put('/projects/:id', (resquest, response) => {
+    
+    const {id} = resquest.params;
+
+    console.log(id);
+    
     return response.json([
         'Projeto 10',
         'Projeto 2',
